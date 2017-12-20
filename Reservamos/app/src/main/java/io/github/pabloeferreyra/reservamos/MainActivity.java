f5b9fd82e4ba5cdb77b2411b1fbdde0ce84d4a4c
Pablo Ferreyra <pabloeferreyra@gmail.com>
2017-12-20 18:05:47 -0300

    inicio de lectura de servicios

--- Reservamos/app/src/main/java/io/github/pabloeferreyra/reservamos/MainActivity.java
+++ Reservamos/app/src/main/java/io/github/pabloeferreyra/reservamos/MainActivity.java
@@ -1,9 +1,18 @@
 package io.github.pabloeferreyra.reservamos;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +30,36 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         }, SPLASH_TIMEOUT);
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                URL githubEndpoint = null;
+                HttpsURLConnection myConnection = null;
+                try {
+                    githubEndpoint = new URL("https://api.github.com/");
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    myConnection =
+                            (HttpsURLConnection) githubEndpoint.openConnection();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                myConnection.setRequestProperty("User-Agent", "my-rest-app-v0.1");
+                try {
+                    if (myConnection.getResponseCode() == 200) {
+                        InputStream responseBody = myConnection.getInputStream();
+                        InputStreamReader responseBodyReader =
+                                new InputStreamReader(responseBody, "UTF-8");
+                    } else {
+                        // Error handling code goes here
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
