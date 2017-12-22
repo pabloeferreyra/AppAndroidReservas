3b9a47953d20750bdd6989f7a13a140e864f3d42
Pablo Ferreyra <pabloeferreyra@gmail.com>
2017-12-22 02:37:10 -0300

    servicio inicialmente funcional, falta hacerlo asyncronic para que se puede hacer una busqueda de menos recursos

--- Reservamos/app/src/main/java/io/github/pabloeferreyra/reservamos/MainActivity.java
+++ Reservamos/app/src/main/java/io/github/pabloeferreyra/reservamos/MainActivity.java
@@ -1,65 +1,81 @@
 package io.github.pabloeferreyra.reservamos;
 
-import android.content.Intent;
-import android.os.AsyncTask;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import org.java_websocket.client.WebSocketClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import javax.net.ssl.HttpsURLConnection;
-
 public class MainActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIMEOUT = 5000;
+    private static final String Direccion = "http://192.168.0.5:8081/locales/";
+
+    private WebSocketClient mWebSocketClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(homeIntent);
-                finish();
-            }
-        }, SPLASH_TIMEOUT);
-        AsyncTask.execute(new Runnable() {
-            @Override
-            public void run() {
-                URL githubEndpoint = null;
-                HttpsURLConnection myConnection = null;
-                try {
-                    githubEndpoint = new URL("https://api.github.com/");
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    myConnection =
-                            (HttpsURLConnection) githubEndpoint.openConnection();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
 
-                myConnection.setRequestProperty("User-Agent", "my-rest-app-v0.1");
-                try {
-                    if (myConnection.getResponseCode() == 200) {
-                        InputStream responseBody = myConnection.getInputStream();
-                        InputStreamReader responseBodyReader =
-                                new InputStreamReader(responseBody, "UTF-8");
-                    } else {
-                        // Error handling code goes here
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        getData();
+
+    }
+
+    public void getData(){
+        String dir= "http://192.168.0.5:8081/locales";
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        URL url = null;
+        HttpURLConnection conn;
+        try {
+            url = new URL(dir);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            String inputLine;
+
+            StringBuffer response = new StringBuffer();
+
+            String json = "";
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
             }
-        });
+
+            json = response.toString();
+
+            JSONArray jsonArr = null;
+
+            jsonArr = new JSONArray(json);
+
+            for(int i = 0; i<= jsonArr.length();i++){
+                JSONObject JsonObject = jsonArr.getJSONObject(i);
+                Log.d("SALIDA: ", JsonObject.optString("Direccion"));
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 }
