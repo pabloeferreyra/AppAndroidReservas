985ecff9c23e8911c1bc7733d98c78a605579854
Pablo Ferreyra <pabloeferreyra@gmail.com>
2017-12-30 13:13:08 -0300

    revert para testing

---
+++ Reservamos/app/src/main/java/io/github/pabloeferreyra/reservamos/MainActivity.java
@@ -0,0 +1,81 @@
+package io.github.pabloeferreyra.reservamos;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import org.java_websocket.client.WebSocketClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String Direccion = "http://192.168.0.5:8081/locales/";
+
+    private WebSocketClient mWebSocketClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getData();
+
+    }
+
+    public void getData(){
+        String dir= "http://192.168.0.5:8081/locales";
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        URL url = null;
+        HttpURLConnection conn;
+        try {
+            url = new URL(dir);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            String inputLine;
+
+            StringBuffer response = new StringBuffer();
+
+            String json = "";
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+
+            json = response.toString();
+
+            JSONArray jsonArr = null;
+
+            jsonArr = new JSONArray(json);
+
+            for(int i = 0; i<= jsonArr.length();i++){
+                JSONObject JsonObject = jsonArr.getJSONObject(i);
+                Log.d("SALIDA: ", JsonObject.optString("Direccion"));
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
