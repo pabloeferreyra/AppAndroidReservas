0b9f8fbb993160646cea778f148aa75ba6796b39
Pablo Ferreyra <pabloeferreyra@gmail.com>
2018-01-10 15:26:59 -0300

    a testear eel appcompatibility

---
+++ Xamarin/Reservamos/App.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace Reservamos
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+			MainPage = new HomePage();
+		}
+	}
+}
